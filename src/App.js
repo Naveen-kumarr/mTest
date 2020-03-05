a7fa9da5bb8399fbddc6bd8ebda6bb0574cc112b
naveen <naveen>
2020-03-06 05:00:18 +0530

    Update with three section

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sectionthird from './Components/Sectionthird'
+import Sectionfourth from './Components/Sectionfourth'
+import Sectionfifth from './Components/Sectionfifth'
+
+function App() {
+  return (
+    <div>
+<Sectionthird/>
+<Sectionfourth/>
+<Sectionfifth/>
+    </div>
+  );
+}
+
+export default App;
